2c49a7a605b34835ede4afab2f16cd4649841878
rogrocha <rogersdarocha@gmail.com>
2025-01-13 14:14:39 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/LiterALura/LiterALuraApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.LiterALura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterALuraApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LiterALuraApplication.class, args);
+	}
+
+}
